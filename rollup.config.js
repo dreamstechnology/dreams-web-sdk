bdb6bb8af0c0529586134a6c9fbf88556f07c11c
beniutek <bbeniutek@gmail.com>
2021-10-11 11:56:02 +0200

    build: add typescript to rollup build
    it's needed for typedoc
    
    AC-1662

--- rollup.config.js
+++ rollup.config.js
@@ -2,11 +2,11 @@ import resolve from '@rollup/plugin-node-resolve';
 import { babel } from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import pkg from './package.json';
+import typescript from '@rollup/plugin-typescript';
 
 const name = 'DreamsWebSDK';
 const mainFields = ['main', 'module', 'browser']
 const extensions = ['.js', '.ts'];
-
 const babelConfig = {
   babelHelpers: 'bundled',
   include: ['src/**/*'],
@@ -20,20 +20,12 @@ const babelConfig = {
 
 export default {
   input: './src/main.ts',
-
-  // Specify here external modules which you don't want to include in your bundle (for instance: 'lodash', 'moment' etc.)
-  // https://rollupjs.org/guide/en/#external
   external: [],
-
   plugins: [
-    // Allows node_modules resolution
     resolve({ extensions, mainFields }),
-
-    // Allow bundling cjs modules. Rollup doesn't understand cjs
     commonjs(),
-
-    // Compile TypeScript/JavaScript files
     babel(babelConfig),
+    typescript()
   ],
 
   output: [
