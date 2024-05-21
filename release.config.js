8eb2faacfe59ac0236ba47c24d8e23e51f68c48a
Krzysztof Lityński <krzysztof.litynski@dreamstech.com>
2024-05-21 14:55:07 +0200

    build: use ssh for git url

--- release.config.js
+++ release.config.js
@@ -1,25 +1,29 @@
-const commitAnalyzer = ["@semantic-release/commit-analyzer", {
-  "preset": "angular",
-  "releaseRules": [
-    {"type": "docs", "scope":"readme", "release": "patch"},
-    {"type": "refactor", "release": "patch"},
-    {"type": "style", "release": "patch"},
-    {"type": "build", "release": "patch"}
-  ],
-  "parserOpts": {
-    "noteKeywords": ["BREAKING CHANGE", "BREAKING CHANGES"]
-  }
-}];
+const commitAnalyzer = [
+  '@semantic-release/commit-analyzer',
+  {
+    preset: 'angular',
+    releaseRules: [
+      { type: 'docs', scope: 'readme', release: 'patch' },
+      { type: 'refactor', release: 'patch' },
+      { type: 'style', release: 'patch' },
+      { type: 'build', release: 'patch' },
+    ],
+    parserOpts: {
+      noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES'],
+    },
+  },
+];
 
 module.exports = {
   preset: 'angular',
   branches: ['main'],
+  repositoryUrl: 'git@github.com:dreamstechnology/dreams-web-sdk.git',
   plugins: [
     commitAnalyzer,
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
     '@semantic-release/github',
     '@semantic-release/git',
-    '@semantic-release/npm'
+    '@semantic-release/npm',
   ],
 };
